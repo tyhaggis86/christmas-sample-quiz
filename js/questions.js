09941ab904fa6f7414bebe5f3a6781d68b31d40d
tyhaggis86 <113810488+tyhaggis86@users.noreply.github.com>
2022-12-15 11:25:17 +1000

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,167 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Will I get this question right?",
+    answer: "Yes",
+    options: [
+      "Yes",
+      "No",
+      "Maybe",
+      "I don't know"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What about this one?",
+    answer: "No",
+    options: [
+      "Yes",
+      "No",
+      "Maybe",
+      "I don't know"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How about this?",
+    answer: "Maybe",
+    options: [
+      "Yes",
+      "No",
+      "Maybe",
+      "I don't know"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Or this?",
+    answer: "I don't know",
+    options: [
+      "Yes",
+      "No",
+      "Maybe",
+      "I don't know"
+    ]
+  },
+//     {
+//     numb: 5,
+//     question: "What colour Christmas is Elvis going to have?",
+//     answer: "Blue",
+//     options: [
+//       "White",
+//       "Red",
+//       "Blue",
+//       "Green"
+//     ]
+//   },
+//     {
+//     numb: 6,
+//     question: "Which Hollywood actor played six different roles in The Polar Express?",
+//     answer: "Tom Hanks",
+//     options: [
+//       "Tom Hanks",
+//       "Richard Attenborough",
+//       "Tim Allen",
+//       "Bruce Willis"
+//     ]
+//   },
+//   {
+//     numb: 7,
+//     question: "What are you supposed to do when you find yourself under the mistletoe?",
+//     answer: "Kiss",
+//     options: [
+//       "Dance",
+//       "Sing",
+//       "Kiss",
+//       "Run away"
+//     ]
+//   },
+//   {
+//     numb: 8,
+//     question: "Which real-life person is Santa Claus based on?",
+//     answer: "St. Nicholas",
+//     options: [
+//       "St. Christopher",
+//       "St. Bernard",
+//       "St. Jude",
+//       "St. Nicholas"
+//     ]
+//   },
+//   {
+//     numb: 9,
+//     question: "In the movie Elf, what was the first rule of The Code of Elves?",
+//     answer: "Treat everyday like Christmas",
+//     options: [
+//       "Treat everyday like Christmas",
+//       "Check the list twice",
+//       "There is room for everyone on the nice list",
+//       "The BEST way to spread Christmas cheer is singing loud for all to hear"
+//     ]
+//   },
+//   {
+//     numb: 10,
+//     question: "How do you say Merry Christmas in Spanish?",
+//     answer: "Feliz Navidad",
+//     options: [
+//       "Nollaig Shona",
+//       "Feliz Navidad",
+//       "Marau na Kerisimasi",
+//       "Meri Kirihimete"
+//     ]
+//   },
+//   {
+//     numb: 11,
+//     question: "What is the best-selling Christmas song ever?",
+//     answer: "White Christmas by Bing Crosby",
+//     options: [
+//       "All I Want for Christmas Is You by Mariah Carey",
+//       "Do They Know It's Christmas by Band Aid",
+//       "Fairytale of New York by The Pogues",
+//       "White Christmas by Bing Crosby"
+//     ]
+//   },
+//   {
+//     numb: 12,
+//     question: "What was Frosty the Snowman's nose made out?",
+//     answer: "A button",
+//     options: [
+//       "Corn cob pipe",
+//       "A button",
+//       "Coal",
+//       "A carrot"
+//     ]
+//   },
+//   {
+//     numb: 13,
+//     question: "What popular Christmas song was actually written for Thanksgiving?",
+//     answer: "Jingle Bells",
+//     options: [
+//       "Jingle Bells",
+//       "Deck the Halls",
+//       "Joy to the World",
+//       "O Holy Night"
+//     ]
+//   },
+//   {
+//     numb: 14,
+//     question: "What was the first company that used Santa Claus in advertising?",
+//     answer: "Coca Cola",
+//     options: [
+//       "Pepsi",
+//       "Dr. Pepper",
+//       "Coca Cola",
+//       "Mountain Dew"
+//     ]
+//   },
+//   {
+//     numb: 15,
+//     question: "What is The Grinches dog's name?",
+//     answer: "Max",
+//     options: [
+//       "Fido",
+//       "Charlie",
+//       "Buddy",
+//       "Max"
+//     ]
+//   },
+];
